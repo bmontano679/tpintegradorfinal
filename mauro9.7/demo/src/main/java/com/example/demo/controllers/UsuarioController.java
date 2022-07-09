b89e8f54965a55b302a9f523936cf7ca1e11229b
bmontano679 <104459593+bmontano679@users.noreply.github.com>
2022-07-09 19:37:38 -0300

    Add files via upload

---
+++ mauro9.7/demo/src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/usuarios")         // mapea     /usuarios
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+    @CrossOrigin   // para solucionar error “cors”
+    @GetMapping()      //trae todos los usuarios y lo mapea  /usuarios
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+    @CrossOrigin
+    @PostMapping()    //   graba un usuario
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+ 
+    @CrossOrigin
+    @GetMapping( path = "/{id}")    // trae un usuario segun id
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+ 
+    @CrossOrigin
+    @GetMapping("/query")        // trae los usuarios con determinada prioridad
+    public ArrayList<UsuarioModel> obtenerUsuarioPorConsulta(@RequestParam("consulta") String consulta){
+        return this.usuarioService.obtenerPorConsulta(consulta);
+    }}
---
+++ mauro9.7/main.js
@@ -0,0 +1,44 @@
+
+var data = { 
+    cursos: [
+        {
+    "name": "Nena",
+    "Tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/animalitos/20200810_180558.jpg"
+    }
+  ,
+
+      {
+    "name": "Nena",
+    "Tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/animalitos/20210623_192901.jpg"
+    }
+    ,
+    {
+        "name": "Pancho",
+        "Tamaño": "21cm * 29.7cm",
+        "precio": 15,
+        "imagen" :"./img/animalitos/PicsArt_06-24-09.37.44.jpg"
+        }
+        ,
+        {
+            "name": "Loca",
+            "Tamaño": "21cm * 29.7cm",
+            "precio": 15,
+            "imagen" :"./img/animalitos/PicsArt_06-25-11.27.10.jpg"
+            }
+            ,
+            {
+                "name": "Doggy",
+                "desc": "Dibujo hecho a mano",
+                "precio": 15,
+                "imagen" :"./img/animalitos/PicsArt_08-12-01.09.51.jpg"
+                }
+]}
+
+var app = new Vue ({
+    el:'#apppedidos',
+    data : data 
+    })
