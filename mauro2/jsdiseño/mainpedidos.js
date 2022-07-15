15cf9b7ad68f2c5212682f787a4750e7cec358e3
bmontano679 <104459593+bmontano679@users.noreply.github.com>
2022-07-15 18:27:09 -0300

    Add files via upload

---
+++ mauro2/jsdiseño/mainpedidos.js
@@ -0,0 +1,16 @@
+
+var data = { 
+    pedidos: [
+        {
+    "name": "Retrato familiar",
+    "tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/pedidos/20201113_175554.jpg"
+    }
+  
+]}
+
+var app = new Vue ({
+    el:'#apppedidos',
+    data : data 
+    })
---
+++ mauro2/mainmascotas.js
@@ -0,0 +1,44 @@
+
+var data = { 
+    cursos: [
+        {
+    "name": "Nena",
+    "tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/animalitos/20200810_180558.jpg"
+    }
+  ,
+
+      {
+    "name": "Nena",
+    "tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/animalitos/20210623_192901.jpg"
+    }
+    ,
+    {
+        "name": "Pancho",
+        "tamaño": "21cm * 29.7cm",
+        "precio": 15,
+        "imagen" :"./img/animalitos/PicsArt_06-24-09.37.44.jpg"
+        }
+        ,
+        {
+            "name": "Loca",
+            "tamaño": "21cm * 29.7cm",
+            "precio": 15,
+            "imagen" :"./img/animalitos/PicsArt_06-25-11.27.10.jpg"
+            }
+            ,
+            {
+                "name": "Doggy",
+                "tamaño": "21cm * 29.7cm",
+                "precio": 15,
+                "imagen" :"./img/animalitos/PicsArt_08-12-01.09.51.jpg"
+                }
+]}
+
+var app = new Vue ({
+    el:'#appmascotas',
+    data : data 
+    })
