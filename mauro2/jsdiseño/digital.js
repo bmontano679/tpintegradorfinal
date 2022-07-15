8063d0f210163e95a87692173b1e2917bb39edf4
bmontano679 <104459593+bmontano679@users.noreply.github.com>
2022-07-15 17:56:32 -0300

    Add files via upload

---
+++ mauro2/jsdiseño/digital.js
@@ -0,0 +1,44 @@
+var data = { 
+    digitales: [
+        {
+    "name": "Nena",
+    "tamaño": "21cm * 29.7cm",
+    "precio": " Consultar precio ",
+    "imagen" :"./img/photoshop_/20220112_181033.jpg"
+    }
+    ,
+
+    {
+  "name": "Pancho",
+  "tamaño": "21cm * 29.7cm",
+  "precio": " Consultar precio ",
+  "imagen" :"./img/photoshop_/Panxo2.0-1.jpg"
+  }
+  ,
+  {
+      "name": "Loca",
+      "tamaño": "21cm * 29.7cm",
+      "precio": " Consultar precio ",
+      "imagen" :"./img/photoshop_/PicsArt_02-08-06.27.53.jpg"
+      }
+      ,
+      {
+          "name": "Coquita",
+          "tamaño": "21cm * 29.7cm",
+          "precio": 15,
+          "imagen" :"img/photoshop_/PicsArt_02-16-05.08.01.jpg"
+          }
+          ,
+          {
+              "name": "Tom Holland",
+              "tamaño": "21cm * 29.7cm",
+              "precio": " Consultar precio ",
+              "imagen" :"./img/photoshop_/PicsArt_03-16-04.47.14.jpg"
+              }
+              
+]}
+
+var app = new Vue ({
+    el:'#appdigitales',
+    data : data 
+    })
---
+++ mauro2/jsdiseño/retratos.js
@@ -0,0 +1,51 @@
+
+var data = { 
+    retratos: [
+        {
+    "name": "Eva De Dominici  ",
+    "tamaño": "21cm * 29.7cm",
+    "precio": 15,
+    "imagen" :"./img/retratos/20201102_173050.jpg"
+    }
+    ,
+
+    {
+  "name": "Juego de gemelas - Lindsay Lohan ",
+  "tamaño": "21cm * 29.7cm",
+  "precio": 15,
+  "imagen" :"./img/retratos/PicsArt_03-24-05.32.00.jpg"
+  }
+  ,
+  {
+      "name": "Juego del calamar - Hoyeon Jung",
+      "tamaño": "21cm * 29.7cm",
+      "precio": 15,
+      "imagen" :"./img/retratos/PicsArt_10-07-06.06.50.jpg"
+      }
+      ,
+      {
+          "name": "You - Penn Badgley",
+          "tamaño": "21cm * 29.7cm",
+          "precio": 15,
+          "imagen" :"./img/retratos/PicsArt_10-19-06.05.18.jpg"
+          }
+          ,
+          {
+              "name": "jojo rabbit - Roman Griffin Davis",
+              "tamaño": "21cm * 29.7cm",
+              "precio": 15,
+              "imagen" :"./img/retratos/PicsArt_11-26-04.11.23.jpg"
+              }
+              ,
+          {
+              "name": "Casa de papel - Rodrigo De La Serna",
+              "tamaño": "21cm * 29.7cm",
+              "precio": 15,
+              "imagen" :"./img/retratos/PicsArt_12-22-06.11.28.jpg"
+              }
+]}
+
+var app = new Vue ({
+    el:'#appretratos',
+    data : data 
+    })
